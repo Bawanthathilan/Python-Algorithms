2ec1bc500253d2b6244bf0b10aa0dfeb05b367df
Bawantha Thilan <38850236+bawantharathnayakasliit@users.noreply.github.com>
2019-09-09 09:18:40 +0530

    update selection sort

--- slection sort.py
+++ slection sort.py
@@ -1,13 +1,13 @@
 import sys
-A=[64,25,12,22,11]
+A=[64,25,12,22,11 , 30]
 
 for i in range(len (A)):
-    min=i
+    min_idx=i
     for j in range(i+1, len (A)):
-        if A[min]>A[j]:
-            min = j
+        if A[min_idx]>A[j]:
+            min_idx = j
 
-A[i] , A[min] =  A[min] , A[i]
+    A[i] , A[min_idx] =  A[min_idx] , A[i]
 
 print("Selection sorted array")
 for i in range(len(A)):
